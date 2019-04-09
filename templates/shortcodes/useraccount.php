4c39b1e1f3a7b85706d057da8be74d3885698327
Jonathan Stegall <jstegall@minnpost.com>
2019-04-09 11:37:41 -0500

    start shortcodes for user account forms for mailchimp

---
+++ templates/shortcodes/useraccount.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for managing settings in user's account
+ *
+ */
+?>
+user account settings for mailchimp
---
+++ templates/shortcodes/usersummary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for showing a logged in user their current MailChimp settings
+ *
+ */
+?>
+user account summary for mailchimp
