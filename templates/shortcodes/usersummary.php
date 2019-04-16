27a41164b50dc28a35018a1d357a0f79d7c10c8d
Jonathan Stegall <jstegall@minnpost.com>
2019-04-16 13:56:02 -0500

    user account summary

--- templates/shortcodes/usersummary.php
+++ templates/shortcodes/usersummary.php
@@ -4,4 +4,13 @@
  *
  */
 ?>
-user account summary for mailchimp
+<?php if ( ! empty( $form['group_fields'] ) ) : ?>
+	<dl class="a-user-emails">
+		<?php foreach ( $form['group_fields'] as $category ) : ?>
+			<dt><?php echo $category['name']; ?></dt>
+			<?php if ( isset( $category['contains'] ) ) : ?>
+				<dd><?php echo implode( ', ', array_column( $category[ $category['contains'] ], 'name' ) ); ?></dd>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</dl>
+<?php endif; ?>
