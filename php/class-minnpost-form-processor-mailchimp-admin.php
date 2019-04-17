08e22fc5cdbf14a1e0e777dabb3fdc8ba2406b8d
Jonathan Stegall <jstegall@minnpost.com>
2019-04-16 22:46:16 -0500

    sanitize bug

--- php/class-minnpost-form-processor-mailchimp-admin.php
+++ php/class-minnpost-form-processor-mailchimp-admin.php
@@ -730,7 +730,7 @@ class MinnPost_Form_Processor_MailChimp_Admin {
 				esc_attr( $value ),
 				esc_attr( $name ),
 				esc_attr( $id ),
-				sanitize_html_class( $class, esc_html( ' code' ) ),
+				$this->sanitize_html_classes( $class, esc_html( ' code' ) ),
 				esc_html( $checked )
 			);
 			if ( '' !== $desc ) {
@@ -767,7 +767,7 @@ class MinnPost_Form_Processor_MailChimp_Admin {
 			echo sprintf( '<textarea name="%1$s" id="%2$s" class="%3$s" rows="10">%4$s</textarea>',
 				esc_attr( $name ),
 				esc_attr( $id ),
-				sanitize_html_class( $class . esc_html( ' code' ) ),
+				$this->sanitize_html_classes( $class . esc_html( ' code' ) ),
 				esc_attr( $value )
 			);
 			if ( '' !== $desc ) {
@@ -964,6 +964,19 @@ class MinnPost_Form_Processor_MailChimp_Admin {
 			);
 		}
 
+	}
+
+	private function sanitize_html_classes( $classes, $sep = ' ' ) {
+		$return = '';
+		if ( ! is_array( $classes ) ) {
+			$classes = explode( $sep, $classes );
+		}
+		if ( ! empty( $classes ) ) {
+			foreach ( $classes as $class ) {
+				$return .= sanitize_html_class( $class ) . ' ';
+			}
+		}
+		return $return;
 	}
 
 }
