cc0250a32e306b27a812fe7e8bdbbaf2282d3a9b
Jonathan Stegall <jstegall@minnpost.com>
2019-05-21 09:36:00 -0500

    when a form is submitted, check for honeypot fields

--- php/class-minnpost-form-processor-mailchimp-post-data.php
+++ php/class-minnpost-form-processor-mailchimp-post-data.php
@@ -119,8 +119,47 @@ class MinnPost_Form_Processor_MailChimp_Post_Data {
 				$user_data['last_name'] = $last_name;
 			}
 
-			// mailchimp fields
-			$result = $this->save_to_mailchimp( $action, $resource_type, $resource_id, $subresource_type, $user_data );
+			// honeypot checker
+			$honeypot_name    = isset( $_POST['mhp_name'] ) ? esc_attr( $_POST['mhp_name'] ) : '';
+			$honeypot_email   = isset( $_POST['mhp_email'] ) ? esc_attr( $_POST['mhp_email'] ) : '';
+			$honeypot_comment = isset( $_POST['mhp_comment'] ) ? esc_attr( $_POST['mhp_comment'] ) : '';
+
+			if ( '' !== $honeypot_name || '' !== $honeypot_email || '' !== $honeypot_comment ) {
+				$result          = array(
+					'status' => 'spam',
+					'local'  => true,
+				);
+				$confirm_message = __( 'Our system did not allow this submission because it has characteristics of a spambot submission.', 'minnpost-form-processor-mailchimp' );
+
+				$log_title   = sprintf(
+					esc_html__( 'MailChimp Send Error: Honeypot fields had values', 'form-processor-mailchimp' )
+				);
+				$log_message = sprintf(
+					'<p>' .
+					// translators: placeholders are: 1) submitted email address, 2) honeypot name, 3) honeypot email, 4) honeypot comment
+					esc_html__( 'Email address was %1$s.', 'form-processor-mailchimp' ) .
+					'</p>' .
+					'<ul><li><strong>mhp_name:</strong> %2$s</li><li><strong>mhp_email:</strong> %3$s</li><li><strong>mhp_comment:</strong> %4$s</li></ul>',
+					esc_html( $_POST['email'] ),
+					esc_html( $_POST['mhp_name'] ),
+					esc_html( $_POST['mhp_email'] ),
+					esc_html( $_POST['mhp_comment'] )
+				);
+
+				$log_entry = array(
+					'title'   => $log_title,
+					'message' => $log_message,
+					'trigger' => 0,
+					'parent'  => '',
+					'status'  => 'spam',
+				);
+
+				$this->parent->logging->setup( $log_entry );
+
+			} else {
+				// save to mailchimp
+				$result = $this->save_to_mailchimp( $action, $resource_type, $resource_id, $subresource_type, $user_data );
+			}
 
 			if ( isset( $result['id'] ) ) {
 				if ( 'PUT' === $result['method'] ) {
@@ -168,11 +207,12 @@ class MinnPost_Form_Processor_MailChimp_Post_Data {
 					$confirm_message = $result['detail'];
 				}
 				if ( isset( $_POST['ajaxrequest'] ) && 'true' === $_POST['ajaxrequest'] ) {
+					$local_error = ( true === $result['local'] ) ? true : false;
 					wp_send_json_error(
 						array(
 							'id'              => isset( $result['id'] ) ? $result['id'] : '',
 							'user_status'     => $user_status,
-							'confirm_message' => $this->get_data->get_error_message( $confirm_message, '', true ),
+							'confirm_message' => $this->get_data->get_error_message( $confirm_message, '', true, $local_error ),
 						)
 					);
 				} else {
